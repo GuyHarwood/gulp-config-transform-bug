474c1fcc9228fe881a1d443920f3c5c18449dc7a
Guy Harwood <gpharwood@gmail.com>
2016-05-05 16:02:18 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var
+    gulp = require('gulp'),
+    configTransform = require('gulp-config-transform');
+
+if (true === true) {
+    var gulp_src = gulp.src;
+    gulp.src = function () {
+        return gulp_src.apply(gulp, arguments)
+            .pipe(plumber(function (error) {
+                // Output an error message
+                gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+                // emit the end event, to properly end the task
+                this.emit('end');
+            })
+            );
+    };
+}
+
+gulp.task('default', function () {
+    var options = { 
+        transform: 'Web.Release.config',
+        config: 'Web.Config',
+        destination: 'My.Config',
+        assemblyFile: 'C:\\Program Files (x86)\\MSBuild\\Microsoft\\VisualStudio\\v14.0\\Web\\Microsoft.Web.Publishing.Tasks.dll'
+     };
+    var result = configTransform(options);
+    //return gulp.dest(result);
+});
