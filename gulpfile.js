79cfbbedfd554e5b32cbca7e6da7f4760eb1bebb
Guy Harwood <gpharwood@gmail.com>
2016-05-12 09:36:18 +0100

    fix errors in transform file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var
     gulp = require('gulp'),
-    configTransform = require('gulp-config-transform');
+    configTransform = require('gulp-config-transform'),
+    argv = require('yargs').argv;
+    
+var errors = argv.errors || false;
 
-if (true === false) {
+if (errors) {
     var gulp_src = gulp.src;
     gulp.src = function () {
         return gulp_src.apply(gulp, arguments)
@@ -23,6 +26,5 @@ gulp.task('default', function () {
         destination: './My.Config'
         // ,assemblyFile: 'C:\\Program Files (x86)\\MSBuild\\Microsoft\\VisualStudio\\v14.0\\Web\\Microsoft.Web.Publishing.Tasks.dll'
      };
-    var result = configTransform(options);
-    //return gulp.dest(result);
+    configTransform(options);
 });
