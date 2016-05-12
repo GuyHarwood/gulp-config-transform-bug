8ea9c0d8292df71ef17bc79eecc9f3e9ea17df34
Guy Harwood <gpharwood@gmail.com>
2016-05-12 09:53:32 +0100

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,13 @@
 var
     gulp = require('gulp'),
-    configTransform = require('gulp-config-transform'),
-    argv = require('yargs').argv;
-    
-var errors = argv.errors || false;
-
-if (errors) {
-    var gulp_src = gulp.src;
-    gulp.src = function () {
-        return gulp_src.apply(gulp, arguments)
-            .pipe(plumber(function (error) {
-                // Output an error message
-                gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
-                // emit the end event, to properly end the task
-                this.emit('end');
-            })
-            );
-    };
-}
+    configTransform = require('gulp-config-transform');
 
 gulp.task('default', function () {
     var options = { 
         transform: './Web.Release.config',
         config: './Web.Config',
         destination: './My.Config'
-        // ,assemblyFile: 'C:\\Program Files (x86)\\MSBuild\\Microsoft\\VisualStudio\\v14.0\\Web\\Microsoft.Web.Publishing.Tasks.dll'
+        //,assemblyFile: 'C:\\Program Files (x86)\\MSBuild\\Microsoft\\VisualStudio\\v14.0\\Web\\Microsoft.Web.Publishing.Tasks.dll'
      };
     configTransform(options);
 });
